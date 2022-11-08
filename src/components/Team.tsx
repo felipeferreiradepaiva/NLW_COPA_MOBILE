fa141b53b1b3a3c11de111caddf3c1e6c7469441
Felipe Paiva <ffpaiva@outlook.com>
2022-11-08 10:59:45 -0300

    feature: add Load game in Detail screen

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -5,24 +5,36 @@ import { Input } from './Input';
 
 interface Props {
   code: string;
+  value?: number;
   position: 'left' | 'right';
   onChangeText: (value: string) => void;
 }
 
-export function Team({ code, position, onChangeText }: Props) {
+export function Team({ code, value, position, onChangeText }: Props) {
   return (
     <HStack alignItems="center">
       {position === 'left' && <CountryFlag isoCode={code} size={25} style={{ marginRight: 12 }} />}
 
+      {!value &&       
       <Input
         w={10}
         h={9}
         textAlign="center"
         fontSize="xs"
         keyboardType="numeric"
-        onChangeText={onChangeText}
+        onChangeText={onChangeText}        
       />
-
+      || 
+      <Input
+        w={10}
+        h={9}
+        textAlign="center"
+        fontSize="xs"
+        keyboardType="numeric"
+        value={value.toString()}
+        isReadOnly={true}
+      />     
+      }
       {position === 'right' && <CountryFlag isoCode={code} size={25} style={{ marginLeft: 12 }} />}
     </HStack>
   );
